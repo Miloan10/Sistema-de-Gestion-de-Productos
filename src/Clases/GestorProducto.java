f1bb03f788fc2096c62051d48d5a36e455187dc1
danny06javier-c <danny06javier@gmail.com>
2026-09-24 19:39:44 -0500

    agregue la clase gestorproducto y parte de la clase producto

--- src/Clases/GestorProducto.java
+++ src/Clases/GestorProducto.java
@@ -1,5 +1,55 @@
 package Clases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorProducto {
+ private static Producto Producto;
+    private static final List<Producto> listaProductos = new ArrayList<>();
     
+    public void agregarProducto(int id, String nombre, double precio) {
+        for (Producto producto : listaProductos) {
+            if (producto.getId() == id) {
+                System.err.println("EL ID YA EXISTE");
+                return;
+            }
+        }
+        Producto = new Producto(id, nombre, precio);
+        listaProductos.add(Producto);
+    }
+    
+    public void mostrarProductos() {
+        if (listaProductos.isEmpty()) {
+            System.err.println("SIN PRODUCTOS");
+        } else {
+            for (Producto producto : listaProductos) {
+                System.out.println(producto);
+            }
+        }
+    }
+    
+    public void actualizarPrecioProducto(int id, double precio) {
+        for (Producto producto : listaProductos) {
+            if (producto.getId() == id) {
+                producto.setPrecio(precio);
+                System.out.println("PRECIO ACTUALIZADO");
+                return;
+            }
+        }
+        System.err.println("EL PRODUCTO NO EXISTE");
+    }
+    
+    public void eliminarProducto(int id) {
+
+        for (Producto producto : listaProductos) {
+            if (producto.getId() == id) {
+                listaProductos.remove(producto);
+                System.out.println("PRODUCTO ELIMINADO CORRECTAMENTE");
+                return;
+            }
+        }
+
+        System.err.println("EL PRODUCTO NO EXISTE");
+    
+    }
 }
